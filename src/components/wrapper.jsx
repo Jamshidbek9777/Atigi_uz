138f71648b933fc2404489f9ee062ed0d9b3726f
Jamshidbek9777 <jamshidbekrasulovuz@gmail.com>
2025-01-28 01:00:36 +0500

    wrapper 1440px

--- src/components/wrapper.jsx
+++ src/components/wrapper.jsx
@@ -1,6 +1,6 @@
 function Wrapper({ children }) {
   return (
-    <div className="max-w-[1740px] px-[20px] w-full mx-auto">{children}</div>
+    <div className="max-w-[1440px] px-[20px] w-full mx-auto">{children}</div>
   );
 }
 
